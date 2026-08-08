d33dc1cd78d519e8857bd791bed29676e4636495
Aishwarya kadu <146860841+Sjrgjsk@users.noreply.github.com>
2026-08-08 18:02:44 +0530

    Add files via upload

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,1 +1,30 @@
+﻿using CareerConnect.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
+namespace CareerConnect.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Company> Companies { get; set; }
+
+        public DbSet<Job> Jobs { get; set; }
+
+        public DbSet<Application> Applications { get; set; }
+
+        public DbSet<Resume> Resumes { get; set; }
+
+        public DbSet<SavedJob> SavedJobs { get; set; }
+
+        public DbSet<Feedback> Feedbacks { get; set; }
+
+        public DbSet<Profile> Profiles { get; set; }
+    }
+}
---
+++ Data/DbIntializer.cs
@@ -0,0 +1,55 @@
+﻿using CareerConnect.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace CareerConnect.Data
+{
+    public static class DbInitializer
+    {
+        public static async Task SeedRolesAndAdminAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            string[] roles =
+            {
+                "Admin",
+                "Recruiter",
+                "JobSeeker"
+            };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            // Create Default Admin User
+            string adminEmail = "admin@careerconnect.com";
+            string adminPassword = "Admin@123";
+
+            var adminUser = await userManager.FindByEmailAsync(adminEmail);
+
+            if (adminUser == null)
+            {
+                adminUser = new ApplicationUser
+                {
+
+
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    FullName = "System Administrator",
+                    EmailConfirmed = true
+                };
+
+                var result = await userManager.CreateAsync(adminUser, adminPassword);
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(adminUser, "Admin");
+                }
+            }
+        }
+    }
+}
